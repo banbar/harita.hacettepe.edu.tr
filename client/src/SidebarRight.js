be7caa782afefc23a7537055bc7a247ed4e40c73
RAFET <75682122+cirpanrafet@users.noreply.github.com>
2023-06-15 19:02:47 +0300

    SidebarRight.js

--- client/src/SidebarRight.js
+++ client/src/SidebarRight.js
@@ -5,6 +5,8 @@ const SidebarRight = ({ isOpen, onClick }) => {
   const logoSrc = isOpen ? process.env.PUBLIC_URL + '/assets/logos/left.png' : process.env.PUBLIC_URL + '/assets/logos/right.png';
 
   return (
+    // Sağ kenar çubuğunu temsil eden div elementi
+    // Div element representing the right sidebar
     <div className="sidebarRight" onClick={onClick} style={{ left: isOpen ? '303px' : '0' }}>
       <img id="logo" src={logoSrc} alt="" />
     </div>
