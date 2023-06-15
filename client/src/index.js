3096013a755afd6cc3eaafaf43deeab5fde14e07
RAFET <75682122+cirpanrafet@users.noreply.github.com>
2023-06-15 18:48:08 +0300

    index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./index.css";
 
-
-
-
-
+// 'root' elementini hedefleyerek bir ReactDOM kökü oluştur
+// Create a ReactDOM root by targeting the 'root' element
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// ReactDOM kökünde uygulamayı render et
+// render app in ReactDOM root
 root.render(
   <React.StrictMode>
     <App />
