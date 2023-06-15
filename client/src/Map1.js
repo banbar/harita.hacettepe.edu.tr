db9f9b21399bdc63fefee4ff73b925b50b835945
RAFET <75682122+cirpanrafet@users.noreply.github.com>
2023-06-15 18:54:43 +0300

    Map1.js

--- client/src/Map1.js
+++ client/src/Map1.js
@@ -7,13 +7,21 @@ function ChangeView({ center, zoom, bounds, selectedBuilding }) {
 
   useEffect(() => {
     if (selectedBuilding) {
+      // Seçilen binanın konumu kullanılarak bir dizi oluşturulur
+      // An array is created using the location of the selected building
       const buildingPosition = [selectedBuilding.latitude, selectedBuilding.longitude];
       const buildingZoom = 18; // İstediğiniz zoom seviyesini burada belirleyebilirsiniz
-
+      
+      // Harita görünümü seçilen binanın konumu ve zoom seviyesine ayarlanır
+      // The map view is adjusted to the position and zoom level of the selected building
       map.setView(buildingPosition, buildingZoom);
     } else if (bounds) {
+      // Eğer sınırlar belirtilmişse, harita bu sınırlara sığacak şekilde ayarlanır
+      // If boundaries are specified, the map is adjusted to fit within those boundaries
       map.fitBounds(bounds);
     } else {
+      // Hiçbir seçenek belirtilmemişse, harita merkez konum ve zoom seviyesine ayarlanır
+      // If no options are specified, the map is set to the center position and zoom level
       map.setView(center, zoom);
     }
   }, [center, zoom, bounds, map, selectedBuilding]);
@@ -24,13 +32,20 @@ function ChangeView({ center, zoom, bounds, selectedBuilding }) {
 function MapComponent({ routeGeometry, userLat, userLng, selectedBuilding }) {
   const center = [39.86559212247091, 32.73417273791947];
   const zoom = 17;
-
+  // Rota geometrisini koordinatlara dönüştürme
+  // Converting route geometry to coordinates
   const routePositions = routeGeometry.map((coord) => [coord[1], coord[0]]);
-
+  
+  // Rota sınırlarını hesaplama
+  // Calculating route boundaries
   const bounds = routePositions.length > 0 ? L.latLngBounds(routePositions) : null;
+  // Başlangıç ve bitiş konumlarını belirleme
+  // Determine the start and end positions
   const startPosition = routePositions.length > 0 ? routePositions[0] : null;
   const endPosition = routePositions.length > 0 ? routePositions[routePositions.length - 1] : null;
 
+  // Başlangıç, bitiş ve kullanıcı konumu için ikonlar oluşturma
+  // Create icons for start, end and user location
   const startIcon = new Icon({
     iconUrl: 'assets/logos/location.png',
     iconSize: [24, 24],
@@ -56,7 +71,9 @@ function MapComponent({ routeGeometry, userLat, userLng, selectedBuilding }) {
     iconUrl: 'assets/logos/location.png',
     iconSize: [36, 36],
   });
-
+  
+  // Harita bileşenini oluşturma ve yapılandırma
+  // Create and configure the map component
   return (
     <MapContainer center={center} zoom={zoom} style={{ width: '100%', height: '100%' }}>
       <ChangeView center={center} zoom={zoom} bounds={bounds} selectedBuilding={selectedBuilding} />
